4c7624f1d45288263bda84529dae4f65a0e90b5a
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-11-17 19:55:08 +0000

    First version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "my", "their", "his"];
+  let adj = ["enormous", "gorgeous", "big", "little"];
+  let noun = ["apple", "chair", "face", "road"];
+  let domain = [".com", ".io", ".net", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let y = 0; y < domain.length; y++) {
+          console.log(`${pronoun[i]}${adj[x]}${noun[j]}${domain[y]}`);
+        }
+      }
+    }
+  }
 };
