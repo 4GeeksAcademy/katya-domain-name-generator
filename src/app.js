509d39e31c1c8706a165dd6fe9e4ea4da3727b42
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-11-30 18:20:25 +0000

    Added button

--- src/app.js
+++ src/app.js
@@ -11,16 +11,20 @@ window.onload = function() {
   let noun = ["apple", "chair", "face", "road"];
   let domain = [".com", ".io", ".net", ".org"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adj.length; x++) {
-      for (let j = 0; j < noun.length; j++) {
-        for (let y = 0; y < domain.length; y++) {
-          let list = document.createElement("li");
-          list.className = "text-primary";
-          list.innerText = `${pronoun[i]}${adj[x]}${noun[j]}${domain[y]}`;
-          document.querySelector("#domains").appendChild(list);
+  document
+    .querySelector("#generatorButton")
+    .addEventListener("click", function(event) {
+      for (let i = 0; i < pronoun.length; i++) {
+        for (let x = 0; x < adj.length; x++) {
+          for (let j = 0; j < noun.length; j++) {
+            for (let y = 0; y < domain.length; y++) {
+              let list = document.createElement("li");
+              list.className = "text-primary";
+              list.innerText = `${pronoun[i]}${adj[x]}${noun[j]}${domain[y]}`;
+              document.querySelector("#domains").appendChild(list);
+            }
+          }
         }
       }
-    }
-  }
+    });
 };
